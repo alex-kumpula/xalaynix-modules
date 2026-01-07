22f978ff35ab0b826b3042fde1adb273c48eff52
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-06 23:24:41 -0700

    fix

--- src/modules/boot/bootDevice.nix
+++ src/modules/boot/bootDevice.nix
@@ -4,7 +4,7 @@
   { options, config, lib, ... }: 
   {
     options.xalaynix.bootDevice = lib.mkOption {
-      type = lib.types.string;
+      type = lib.types.str;
       description = '' 
         The path of the device that the bootloader should reside on.
         Example: "/dev/vda"
