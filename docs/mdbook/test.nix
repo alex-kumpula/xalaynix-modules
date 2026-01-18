1ab3a61ebff14f511e0d23c83a187bd63423f8ba
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-18 00:45:20 -0700

    test

--- docs/mdbook/test.nix
+++ docs/mdbook/test.nix
@@ -33,7 +33,7 @@
         cp book.toml build/
         cp -r src/* build/src/ || true
 
-        cp ${optionsJSON}/share/doc/nixos/options.json build/src/options.json
+        cp ${optionsJSON} build/src/options.json
 
         # cp ${"optionsMarkdown"} build/src/options.md
         # cd build
