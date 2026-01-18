807db62f32a0fea2521bf0c523f424c60c32f2cf
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-18 00:26:00 -0700

    test

--- flake/lib.nix
+++ flake/lib.nix
@@ -1,14 +1,8 @@
 { inputs, config, lib, flake-parts-lib, ... }:
 {
-
-  # Maybe need the below nix code? 
-  # Was in https://github.com/GaetanLepage/nix-config/blob/c97af82e25d96d720d9871c4d31d1479a7d3667e/modules/flake/home-manager.nix
-  # But I don't know if it is really needed.
-  # Leaving this note here just in case.
-
-  # Required to define `homeConfigurations` in multiple files.
+  # Required to define `lib` in multiple files.
   # Otherwise:
-  #   The option `flake.homeConfigurations' is defined multiple times while it's expected to be unique.
+  #   The option `flake.lib' is defined multiple times while it's expected to be unique.
   options = {
     flake = flake-parts-lib.mkSubmoduleOptions {
       lib = lib.mkOption {
