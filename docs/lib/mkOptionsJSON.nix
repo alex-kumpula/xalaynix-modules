265c4a92e71793870c8018b22b6b4d0eae7133dd
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-18 00:40:03 -0700

    test

--- docs/lib/mkOptionsJSON.nix
+++ docs/lib/mkOptionsJSON.nix
@@ -6,21 +6,23 @@
     let
       optionsDoc = pkgs.nixosOptionsDoc {
         inherit (module) options;
-        transformOptions = opt:
-          if opt ? declarations then
-            let
-              root = toString flakeRoot;
-              toGitDecl = decl:
-                let
-                  declStr = toString decl;
-                  pathPart = lib.head (lib.splitString " via option " declStr);
-                in {
-                  path = if lib.hasPrefix root pathPart 
-                          then lib.removePrefix "/" (lib.removePrefix root pathPart)
-                          else pathPart;
-                };
-            in opt // { declarations = map toGitDecl opt.declarations; }
-          else opt;
+
+        # transformOptions = opt:
+        #   if opt ? declarations then
+        #     let
+        #       root = toString flakeRoot;
+        #       toGitDecl = decl:
+        #         let
+        #           declStr = toString decl;
+        #           pathPart = lib.head (lib.splitString " via option " declStr);
+        #         in {
+        #           path = if lib.hasPrefix root pathPart 
+        #                   then lib.removePrefix "/" (lib.removePrefix root pathPart)
+        #                   else pathPart;
+        #         };
+        #     in opt // { declarations = map toGitDecl opt.declarations; }
+        #   else opt;
+        
       };
     in "${optionsDoc.optionsJSON}/share/doc/nixos/options.json";
   };
