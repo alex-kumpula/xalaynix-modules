f588059ebbc75023cbac8dbe8df7f57805c1bfd5
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-17 23:22:08 -0700

    test

--- docs/lib/test.nix
+++ docs/lib/test.nix
@@ -1,19 +1,26 @@
 { inputs, ... }:
 {
-  # perSystem = { config, pkgs, system, ... }: {
-  #   packages.docs2 = inputs.self.lib.mkDocs pkgs.mkOptionsDoc {
-  #     name = "xalaynix-manual";
-  #     src = ./docs; 
-  #     modulePrefix = "xalaynix";
-  #     flakeRoot = inputs.self;
-  #     githubInfo = {
-  #       user = "alex-kumpula";
-  #       repo = "xalaynix-modules";
-  #       branch = "main";
-  #     };
-  #     module = inputs.nixpkgs.lib.evalModules {
-  #       modules = [ inputs.self.modules.nixos.xalaynix { _module.check = false; } ];
-  #     };
-  #   };
-  # };
+  perSystem = { config, pkgs, lib, system, ... }: {
+    packages.docs2 = 
+      let 
+        xalaynixLib = inputs.self.lib { inherit lib pkgs; };
+      in
+      xalaynixLib.mkDocs {
+        name = "xalaynix-manual";
+        src = ./docs; 
+        modulePrefix = "xalaynix";
+        flakeRoot = inputs.self;
+        githubInfo = {
+          user = "alex-kumpula";
+          repo = "xalaynix-modules";
+          branch = "main";
+        };
+        module = inputs.nixpkgs.lib.evalModules {
+          modules = [ 
+            inputs.self.modules.nixos.xalaynix 
+            { _module.check = false; } 
+          ];
+        };
+      };
+  };
 }
