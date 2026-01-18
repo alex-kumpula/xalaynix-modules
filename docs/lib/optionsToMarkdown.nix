324df352f71c0c083d7b38379152ce59ac55c66e
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-18 00:36:35 -0700

    test

--- docs/lib/optionsToMarkdown.nix
+++ docs/lib/optionsToMarkdown.nix
@@ -8,16 +8,50 @@
           to_entries | 
           map(select(.key | startswith("${modulePrefix}"))) | 
           .[] |
+
+          # Process the description to clean up NixOS-specific tags
           (.value.description // "" 
             | gsub("{var}`(?<v>[^`]+)`"; "`\(.v)`") 
             | gsub("{var}(?<v>[a-zA-Z0-9_.-]+)"; "`\(.v)`")
             | gsub("{option}`(?<o>[^`]+)`"; "**\(.o)**")
           ) as $desc |
-          "## \(.key)\n\n" + $desc + "\n\n" +
+
+          "## \(.key)\n\n" +
+          $desc + "\n\n" +
           "Type: `\(.value.type)`\n\n" +
-          (if .value.default != null then "Default: `\(if .value.default | type == "object" then .value.default.text else .value.default end)`\n\n" else "" end) +
-          (if (.value.declarations | length > 0) then "Declared by:\n\n" + (.value.declarations | map("- [\(.path)](https://github.com/${githubInfo.user}/${githubInfo.repo}/blob/${githubInfo.branch}/\(.path))") | join("\n")) + "\n\n" else "" end)
+          (if .value.default != null then
+            "Default: `\(if .value.default | type == "object" then .value.default.text else .value.default end)`\n\n"
+          else "" end) +
+          (if (.value.declarations | length > 0) then
+            "Declared by:\n\n" +
+            (.value.declarations | map("- [\(.name | sub(",$"; ""))](\(.url | sub(",$"; "")))") | join("\n")) + "\n\n"
+          else "" end)
         ' ${optionsJSON} > $out
       '';
   };
 }
+
+
+# jq -r '
+#   to_entries | 
+#   map(select(.key | startswith("${moduleToDocument}"))) | 
+#   .[] |
+  
+#   # Process the description to clean up NixOS-specific tags
+#   (.value.description // "" 
+#     | gsub("{var}`(?<v>[^`]+)`"; "`\(.v)`") 
+#     | gsub("{var}(?<v>[a-zA-Z0-9_.-]+)"; "`\(.v)`")
+#     | gsub("{option}`(?<o>[^`]+)`"; "**\(.o)**")
+#   ) as $desc |
+
+#   "## \(.key)\n\n" +
+#   $desc + "\n\n" +
+#   "Type: `\(.value.type)`\n\n" +
+#   (if .value.default != null then
+#     "Default: `\(if .value.default | type == "object" then .value.default.text else .value.default end)`\n\n"
+#   else "" end) +
+#   (if (.value.declarations | length > 0) then
+#     "Declared by:\n\n" +
+#     (.value.declarations | map("- [\(.name | sub(",$"; ""))](\(.url | sub(",$"; "")))") | join("\n")) + "\n\n"
+#   else "" end)
+# ' build/src/options.json > build/src/options.md
