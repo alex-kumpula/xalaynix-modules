3782b80762c82de380d617f24c9be8d189103ea3
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-18 22:32:54 -0700

    fix

--- src/modules/xalaynix.nix
+++ src/modules/xalaynix.nix
@@ -1,8 +1,29 @@
-{ self, lib, ... }:
+{ inputs, ... }:
 {
-  flake.modules.nixos.xalaynix = 
-  { config, lib, ... }: 
+  flake.modules.generic.xalaynix = 
+  { lib, ... }: 
   {
-    
+    options.xalaynix = lib.mkOption {
+      type = lib.types.submodule {
+        options = { };
+      }; 
+      
+      description = ''
+        Options for the xalaynix module and its sub-modules.
+      '';
+      default = {};
+    };
+  };
+
+  flake.modules.nixos.xalaynix = {
+    imports = [
+      inputs.self.modules.generic.xalaynix
+    ];
+  };
+
+  flake.modules.homeManager.xalaynix = {
+    imports = [
+      inputs.self.modules.generic.xalaynix
+    ];
   };
 }
