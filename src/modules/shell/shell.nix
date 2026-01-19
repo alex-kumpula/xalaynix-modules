18000a4742f65f90a1274f465109d6b938418b1b
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-18 22:08:27 -0700

    test

--- src/modules/shell/shell.nix
+++ src/modules/shell/shell.nix
@@ -3,7 +3,7 @@
   flake.modules.homeManager.xalaynix = 
   { pkgs, config, lib, ... }:
   {
-    options.xalaynix.shell = lib.mkOption {
+    options.xalaynix.shell.test = lib.mkOption {
       type = lib.types.attrs;
       description = "Configuration options for shell integrations.";
       default = { };
