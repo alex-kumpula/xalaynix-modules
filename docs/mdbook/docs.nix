9e37a98e30f4ac994b9d2959203d70fd8565fde3
Alex Kumpula <alex.kumpula01@gmail.com>
2026-01-18 22:06:36 -0700

    test

--- docs/mdbook/docs.nix
+++ docs/mdbook/docs.nix
@@ -74,31 +74,3 @@
     };
   };
 }
-
-
-
-
-  #   packages.docs = inputs.self.lib.mkOptionsBook {
-  #     inherit pkgs lib;
-
-  #     flakeRoot = inputs.self;
-
-  #     src = ./.; # Path to the folder containing the mdBook source files.
-
-  #     module = lib.evalModules {
-  #       modules = [ 
-  #         inputs.self.modules.nixos.xalaynix 
-  #         { _module.check = false; } 
-  #       ];
-  #     };
-
-  #     githubInfo = { 
-  #       user = "alex-kumpula"; 
-  #       repo = "xalaynix-modules"; 
-  #       branch = "main"; 
-  #     };
-
-  #     includePrefixes = [ "xalaynix" ];
-  #     excludePrefixes = [ "_module" ];
-  #   };
-  # };
--- src/modules/shell/yazi.nix
+++ src/modules/shell/yazi.nix
@@ -3,34 +3,44 @@
   flake.modules.homeManager.xalaynix = 
   { pkgs, config, lib, ... }:
   {
-    programs.yazi = {
-      enable = true;
-      enableBashIntegration = true;
-      enableZshIntegration = true;
+    options.xalaynix.shell.yazi = lib.mkOption {
+      type = lib.types.bool;
+      description = "Whether or not to enable Yazi.";
+      default = { };
     };
 
-    programs.bash = {
-      enable = true;
+    config = lib.mkIf config.xalaynix.shell.yazi {
+      programs.yazi = {
+        enable = true;
+        enableBashIntegration = true;
+        enableZshIntegration = true;
+      };
 
-      # Appended to ~/.bashrc
-      bashrcExtra = ''
-        # Custom stuff here
-        alias ll='ls -lah'
+      programs.bash = {
+        enable = true;
 
-        # Yazi shell wrapper (see the Yazi website Getting Started page)
-        function y() {
-          local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-          yazi "$@" --cwd-file="$tmp"
-          IFS= read -r -d '\0' cwd < "$tmp"
-          [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
-          rm -f -- "$tmp"
-        }
-      '';
+        # Appended to ~/.bashrc
+        bashrcExtra = ''
+          # Custom stuff here
+          alias ll='ls -lah'
 
-      # Appended to ~/.bash_profile
-      profileExtra = ''
-        export EDITOR=nano
-      '';
+          # Yazi shell wrapper (see the Yazi website Getting Started page)
+          function y() {
+            local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+            yazi "$@" --cwd-file="$tmp"
+            IFS= read -r -d '\0' cwd < "$tmp"
+            [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+            rm -f -- "$tmp"
+          }
+        '';
+
+        # Appended to ~/.bash_profile
+        profileExtra = ''
+          export EDITOR=nano
+        '';
+      };
     };
+
+    
   };
 }
